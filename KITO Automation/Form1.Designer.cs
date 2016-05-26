76bcd3b83f351ffce57264c68c547777190994be
Jesper Staun Hansen <jesper@staunhansen.dk>
2016-05-26 14:34:48 +0200

    Version 1

---
+++ KITO Automation/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace KITO_Automation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputFileButton = new System.Windows.Forms.Button();
+            this.OutputFileTextBox = new System.Windows.Forms.TextBox();
+            this.OutputFileButton = new System.Windows.Forms.Button();
+            this.DoTheMap = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.uLineBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tLineBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnDone = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ColumnDo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ColumnFilename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputFileButton
+            // 
+            this.InputFileButton.Location = new System.Drawing.Point(576, 10);
+            this.InputFileButton.Name = "InputFileButton";
+            this.InputFileButton.Size = new System.Drawing.Size(75, 23);
+            this.InputFileButton.TabIndex = 0;
+            this.InputFileButton.Text = "Add file";
+            this.InputFileButton.UseVisualStyleBackColor = true;
+            this.InputFileButton.Click += new System.EventHandler(this.InputFileButton_Click);
+            // 
+            // OutputFileTextBox
+            // 
+            this.OutputFileTextBox.Location = new System.Drawing.Point(69, 219);
+            this.OutputFileTextBox.Name = "OutputFileTextBox";
+            this.OutputFileTextBox.ReadOnly = true;
+            this.OutputFileTextBox.Size = new System.Drawing.Size(497, 20);
+            this.OutputFileTextBox.TabIndex = 2;
+            // 
+            // OutputFileButton
+            // 
+            this.OutputFileButton.Location = new System.Drawing.Point(576, 217);
+            this.OutputFileButton.Name = "OutputFileButton";
+            this.OutputFileButton.Size = new System.Drawing.Size(75, 23);
+            this.OutputFileButton.TabIndex = 3;
+            this.OutputFileButton.Text = "Out folder";
+            this.OutputFileButton.UseVisualStyleBackColor = true;
+            this.OutputFileButton.Click += new System.EventHandler(this.OutputFileButton_Click);
+            // 
+            // DoTheMap
+            // 
+            this.DoTheMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.DoTheMap.Location = new System.Drawing.Point(576, 268);
+            this.DoTheMap.Name = "DoTheMap";
+            this.DoTheMap.Size = new System.Drawing.Size(75, 23);
+            this.DoTheMap.TabIndex = 4;
+            this.DoTheMap.Text = "DO IT";
+            this.DoTheMap.UseVisualStyleBackColor = true;
+            this.DoTheMap.Click += new System.EventHandler(this.DoTheMap_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "zip files|*.zip|All files|*.*";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // keyBox
+            // 
+            this.keyBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.keyBox.Location = new System.Drawing.Point(69, 271);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(183, 20);
+            this.keyBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "XOR Key";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(258, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.uLineBox);
+            this.flowLayoutPanel1.Controls.Add(this.label4);
+            this.flowLayoutPanel1.Controls.Add(this.tLineBox);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(336, 268);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(230, 26);
+            this.flowLayoutPanel1.TabIndex = 13;
+            // 
+            // uLineBox
+            // 
+            this.uLineBox.Location = new System.Drawing.Point(3, 3);
+            this.uLineBox.Name = "uLineBox";
+            this.uLineBox.ReadOnly = true;
+            this.uLineBox.Size = new System.Drawing.Size(100, 20);
+            this.uLineBox.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 3);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "/";
+            // 
+            // tLineBox
+            // 
+            this.tLineBox.Location = new System.Drawing.Point(127, 3);
+            this.tLineBox.Name = "tLineBox";
+            this.tLineBox.ReadOnly = true;
+            this.tLineBox.Size = new System.Drawing.Size(100, 20);
+            this.tLineBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "File ext";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Out folder";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnDone,
+            this.ColumnDo,
+            this.ColumnFilename});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(553, 201);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            // 
+            // ColumnDone
+            // 
+            this.ColumnDone.HeaderText = "Done";
+            this.ColumnDone.Name = "ColumnDone";
+            this.ColumnDone.ReadOnly = true;
+            this.ColumnDone.Width = 50;
+            // 
+            // ColumnDo
+            // 
+            this.ColumnDo.HeaderText = "Do";
+            this.ColumnDo.Name = "ColumnDo";
+            this.ColumnDo.Width = 50;
+            // 
+            // ColumnFilename
+            // 
+            this.ColumnFilename.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ColumnFilename.HeaderText = "File name";
+            this.ColumnFilename.Name = "ColumnFilename";
+            this.ColumnFilename.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 303);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.DoTheMap);
+            this.Controls.Add(this.OutputFileButton);
+            this.Controls.Add(this.OutputFileTextBox);
+            this.Controls.Add(this.InputFileButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(679, 341);
+            this.MinimumSize = new System.Drawing.Size(679, 341);
+            this.Name = "Form1";
+            this.Text = "KITO Zip encryption automation";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InputFileButton;
+        private System.Windows.Forms.TextBox OutputFileTextBox;
+        private System.Windows.Forms.Button OutputFileButton;
+        private System.Windows.Forms.Button DoTheMap;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox uLineBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tLineBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColumnDone;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColumnDo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFilename;
+    }
+}
+
---
+++ KITO Automation/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KITO_Automation
+{
+    public partial class Form1 : Form
+    {
+        const int chunkSize = 65536;
+        bool running = false;
+        string fpath = "";
+        string fext = "";
+        int[] aArr;
+        int num;
+        List<ZipFile> zipFiles = new List<ZipFile>();
+        public Form1()
+        {
+            InitializeComponent();
+            readKey();
+        }
+
+        private void readKey()
+        {
+            string filename = @"automation.cfg";
+            if (File.Exists(filename))
+            {
+                StreamReader sr = new StreamReader("automation.cfg");
+                this.keyBox.Text = sr.ReadLine();
+                this.textBox1.Text = sr.ReadLine();
+                this.OutputFileTextBox.Text = sr.ReadLine();
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    DataGridViewRow row = new DataGridViewRow();
+                    DataGridViewCheckBoxCell c1 = new DataGridViewCheckBoxCell();
+                    DataGridViewCheckBoxCell c2 = new DataGridViewCheckBoxCell();
+                    c2.Value = true;
+                    DataGridViewTextBoxCell c3 = new DataGridViewTextBoxCell();
+                    c3.Value = Path.GetFileName(line);
+                    ZipFile zf = new ZipFile(line, row, c1, c2);
+                    row.Cells.Add(c1);
+                    row.Cells.Add(c2);
+                    row.Cells.Add(c3);
+
+                    dataGridView1.Rows.Add(row);
+                    zipFiles.Add(zf);
+                }
+                sr.Close();
+            }
+            else
+            {
+                this.keyBox.Text = this.textBox1.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filename = @"automation.cfg";
+            StreamWriter sw = new StreamWriter(filename);
+            sw.WriteLine(this.keyBox.Text);
+            sw.WriteLine(this.textBox1.Text);
+            sw.WriteLine(this.OutputFileTextBox.Text);
+            foreach (ZipFile zf in zipFiles)
+            {
+                sw.WriteLine(zf.Filename);
+            }
+            sw.Close();
+        }
+
+        private void InputFileButton_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+        }
+
+        private void OutputFileButton_Click(object sender, EventArgs e)
+        {
+            DialogResult r = this.folderBrowserDialog1.ShowDialog();
+            if (r == System.Windows.Forms.DialogResult.OK)
+            {
+                this.OutputFileTextBox.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void DoTheMap_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                if (zipFiles.Count == 0 || this.OutputFileTextBox.Text == "")
+                {
+                    MessageBox.Show("No zip files or no output folder");
+                    return;
+                }
+                if (!Directory.Exists(this.OutputFileTextBox.Text))
+                {
+                    MessageBox.Show("Output folder does not exist?");
+                    return;
+                }
+                string kString = this.keyBox.Text;
+                if (kString.IndexOfAny(",".ToCharArray()) != -1)
+                {
+                    string[] sArr = kString.Split(",".ToCharArray());
+                    aArr = sArr.Select(x => int.Parse(x.Trim())).ToArray();
+                }
+                if (aArr.Length == 0)
+                {
+                    MessageBox.Show("No xor keys entered.");
+                    return;
+                }
+                num = aArr.Length - 1;
+                fext = textBox1.Text;
+                if (fext.Length == 0)
+                {
+                    MessageBox.Show("No output file extension.");
+                    return;
+                }
+                dataGridView1.ReadOnly = true;
+                bool ok = false;
+                foreach (ZipFile zf in zipFiles)
+                {
+                    if ((bool)zf.Do.Value)
+                        ok = true;
+                    zf.Done.Value = false;
+                }
+                if (!ok)
+                {
+                    MessageBox.Show("Nothing to do.");
+                    dataGridView1.ReadOnly = false;
+                    return;
+                }
+                fpath = this.OutputFileTextBox.Text;
+                running = true;
+                this.DoTheMap.Text = "STOP";
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                this.DoTheMap.Text = "STOPPING";
+                running = false;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                DataGridViewCheckBoxCell c1 = new DataGridViewCheckBoxCell();
+                DataGridViewCheckBoxCell c2 = new DataGridViewCheckBoxCell();
+                c2.Value = true;
+                DataGridViewTextBoxCell c3 = new DataGridViewTextBoxCell();
+                c3.Value = openFileDialog1.SafeFileNames[i];
+                ZipFile zf = new ZipFile(this.openFileDialog1.FileNames[i], row, c1, c2);
+                row.Cells.Add(c1);
+                row.Cells.Add(c2);
+                row.Cells.Add(c3);
+
+                dataGridView1.Rows.Add(row);
+                zipFiles.Add(zf);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            byte[] chunk;
+            int fsize;
+            try
+            {
+                foreach (ZipFile zp in zipFiles)
+                {
+                    if (((bool)zp.Do.Value) == false) continue;
+                    if (!running) break;
+                    FileStream fsr = new FileStream(zp.Filename, FileMode.Open, FileAccess.Read);
+                    BinaryReader br = new BinaryReader(fsr);
+                    FileStream fsw = new FileStream(fpath + "\\" + Path.GetFileNameWithoutExtension(zp.Filename) + "." + fext, FileMode.Create, FileAccess.Write);
+                    BinaryWriter bw = new BinaryWriter(fsw);
+                    zp.SizeTotal = (int)fsr.Length;
+                    fsize = (int)fsr.Length;
+                    backgroundWorker1.ReportProgress(0, zp);
+                    chunk = br.ReadBytes(chunkSize);
+                    int i = 0;
+                    int y = 0;
+                    zp.SizeDone = chunk.Length;
+                    while (chunk.Length > 0)
+                    {
+                        if (!running) break;
+                        for (i = 0; i < chunk.Length; i++)
+                        {
+                            /*if (11987456 == (y + i))
+                                i = i;*/
+                            chunk[i] ^= (byte)(aArr[(y + i) & num]);
+                        }
+                        bw.Write(chunk);
+                        y += chunk.Length;
+                        chunk = br.ReadBytes(chunkSize);
+                        zp.SizeDone += chunk.Length;
+                        backgroundWorker1.ReportProgress(1, zp);
+                    }
+                    backgroundWorker1.ReportProgress(2, zp);
+                    bw.Flush();
+                    bw.Close();
+                    fsw.Close();
+                    br.Close();
+                    fsr.Close();
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Caught an error: " + ee.Message);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ZipFile zf = (ZipFile)e.UserState;
+            switch (e.ProgressPercentage)
+            {
+                case 0:
+                    tLineBox.Text = zf.SizeTotal.ToString();
+                    uLineBox.Text = zf.SizeDone.ToString();
+                    break;
+                case 1:
+                    uLineBox.Text = zf.SizeDone.ToString();
+                    break;
+                case 2:
+                    uLineBox.Text = zf.SizeDone.ToString();
+                    zf.Done.Value = true;
+                    break;
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.dataGridView1.ReadOnly = false;
+            this.DoTheMap.Enabled = true;
+            running = false;
+            this.DoTheMap.Text = "DO IT";
+        }
+
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            ZipFile _zf = null;
+            foreach (ZipFile zf in zipFiles)
+            {
+                if (zf.Row == e.Row)
+                    _zf = zf;
+            }
+            if (_zf != null)
+                zipFiles.Remove(_zf);
+        }
+    }
+
+    class ZipFile
+    {
+        public string Filename;
+        public DataGridViewRow Row;
+        public DataGridViewCheckBoxCell Done;
+        public DataGridViewCheckBoxCell Do;
+        public int SizeTotal;
+        public int SizeDone;
+
+        public ZipFile(string Filename, DataGridViewRow Row, DataGridViewCheckBoxCell c1, DataGridViewCheckBoxCell c2)
+        {
+            this.Filename = Filename;
+            this.Row = Row;
+            this.Done = c1;
+            this.Do = c2;
+            this.SizeTotal = 0;
+            this.SizeDone = 0;
+        }
+    }
+}
